0391f8ca190f9443349c06ac11da1f029601cf7d
Rokas <rookas.rudzenskas@gmail.com>
2022-12-29 17:18:15 +0200

    Rokas

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './screens/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './navigation/**/*.{js,ts,jsx,tsx}',
+    './App.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // ...
+};
